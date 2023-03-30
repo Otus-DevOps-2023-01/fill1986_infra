ef28317198b3ec6cb21dcfa9c8f9302af4d1cb32
EvgenyF <fill1986@yandex.ru>
2023-03-30 21:06:36 +0300

    fixed test warning

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,6 +1,7 @@
 variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
+
 variable subnet_id {
 description = "Subnets for modules"
 }
@@ -8,4 +9,9 @@ description = "Subnets for modules"
 variable db_disk_image {
   description = "Disk image for db app"
   default = "fd8jrok1psa1ru2eg6d3"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
 }
